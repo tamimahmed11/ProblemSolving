7a8e465a1a821e3a1e267bf32de159ffbf840d81
Tamim <tamim22160334@gmail.com>
2024-11-16 13:32:53 +0600

    commit first

---
+++ A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans=(n-1)/2;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
---
+++ A_To_My_Critics.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int ans=a+c;
+        int ans1=a+b;
+        int ans2=b+c;
+        if(ans>=10|| ans1>=10||ans2>=10)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}

d3729ed4f87a3a9d08eaa483c13620c3fb9e4cdf
Tamim <tamim22160334@gmail.com>
2024-11-19 22:07:27 +0600

    commit tamim

--- A_Alice_s_Adventures_in_Chess.cpp
+++ A_Alice_s_Adventures_in_Chess.cpp
@@ -1,12 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
+
+bool willMeetRedQueen(int n, int a, int b, const string& s) {
+    int x = 0, y = 0;
+
+    for (int i = 0; i < 100; ++i) { // Simulate enough steps (n * 10 is enough as n ≤ 10)
+        for (char move : s) {
+            if (move == 'N') ++y;
+            else if (move == 'E') ++x;
+            else if (move == 'S') --y;
+            else if (move == 'W') --x;
+
+            if (x == a && y == b) return true;
+        }
+    }
+    return false;
+}
+
+int main() {
     int t;
-    cin>>t;
-    while (t--)
-    {
-        
+    cin >> t;
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        string s;
+        cin >> s;
+
+        if (willMeetRedQueen(n, a, b, s)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
